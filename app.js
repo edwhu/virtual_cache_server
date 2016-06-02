61dfc0f53fd805730157081fec66cc4e2dd0da48
Edward Hu <hues@usc.edu>
2016-06-02 11:01:03 -0700

    azure launch

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const fs = require('fs');
 const VIDEO = "jellyfish-3-mbps-hd-h264.mkv";
-const DATA = "cxnData.txt"
+const DATA = "cxnData.txt";
+const FILESIZE = 11202628;
 
-let connections_set = new Set();
-app.get('/', (req, res) => {
+fs.writeFileSync(DATA,'');
+//let connections_set = new Set();
+app.get('/download', (req, res) => {
   console.log('a connection!');
-  if(! (connections_set.has(req.headers)) ) {
-    connections_set.add(req.headers);
+  //if(! (connections_set.has(req.headers)) ) {
+  //  connections_set.add(req.headers);
     const headerData = `Connnection:\n ${JSON.stringify(req.headers,null,4)}\n`;
     fs.appendFile(DATA, headerData, err => {
     if(err) return console.log(err);
-  });
-  }
+    });
+  //}
   const options = {
     root: __dirname,
     dotfiles: 'deny',
@@ -22,9 +25,29 @@ app.get('/', (req, res) => {
         'x-sent': true
     }
   };
-  res.download(`./${VIDEO}`,VIDEO);
-});
+  let time = Date.now();
+  res.download(`./${VIDEO}`,VIDEO, err => {
+    time = Date.now() - time;
+    time/=1000; //convert to seconds.
+    console.log(`time elapsed: ${time} seconds`);
+    let ratio = (FILESIZE/1000000)/time;
+    fs.appendFile(DATA, `ratio:\n${ratio} mb/s\n`, err => {
+      if(err) return console.log(err);
+    });
+  });
 
+});
+app.use('/', express.static(path.join(__dirname, 'public')));
 app.listen(8080, () => {
   console.log('Example app listening on port 8080!');
 });
+
+//promise version of fs.stat
+// const fileSize = file => {
+//   return new Promise (( resolve, reject ) => {
+//     fs.stat(file, (err, stat) => {
+//       if(err) return reject(err);
+//       resolve(stat.size);
+//     });
+//   });
+// }
