e5913f472736fbf3329c0f19eb2a27d6347cbe7f
Edward Hu <hues@usc.edu>
2016-06-01 19:23:06 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const VIDEO = "jellyfish-3-mbps-hd-h264.mkv";
+
+
+let connections_set = new Set();
+app.get('/', (req, res) => {
+  console.log('a connection!');
+  if(! (connections_set.has(req.headers)) ) {
+    connections_set.add(req.headers);
+    const headerData = `Connnection:\n ${JSON.stringify(req.headers,null,4)}\n`;
+    fs.appendFile('cxnData.txt', headerData, err => {
+    if(err) return console.log(err);
+  });
+  }
+  const options = {
+    root: __dirname,
+    dotfiles: 'deny',
+    headers: {
+        'x-timestamp': Date.now(),
+        'x-sent': true
+    }
+  };
+  res.download(`./${VIDEO}`,VIDEO);
+});
+
+app.listen(8080, () => {
+  console.log('Example app listening on port 8080!');
+});
