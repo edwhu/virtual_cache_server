d4863b644c97d2330a017c6174ea089124852e68
Edward Hu <hues@usc.edu>
2016-06-02 11:46:03 -0700

    heroku

--- app.js
+++ app.js
@@ -39,8 +39,7 @@ app.get('/download', (req, res) => {
 
 });
 app.use('/', express.static(path.join(__dirname, 'public')));
-let port = process.env.port || 3000;
-app.listen(port, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log(`Example app listening on port ${port}!`);
 });
 
