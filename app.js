3a0ce74c13520f0fe2e943f5e89728acf8b51c03
Edward Hu <hues@usc.edu>
2016-06-16 11:49:50 -0700

    MongoDB

--- app.js
+++ app.js
@@ -7,25 +7,50 @@ const md5File = require('md5-file');
 const app = express();
 const fs = require('fs');
 const multer = require('multer');
+const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');// Use bluebird
 
 const VIDEO = 'jellyfish-3-mbps-hd-h264.mkv';
 const DATA = 'cxnData.txt';
 const FILESIZE = fs.statSync(VIDEO).size;
 const HASH = 'hash.txt';
 
+//SETUP CODE
 app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({
 	 extended: true
 }));
 app.use(bodyParser.json());
-
-
-md5File(`./${VIDEO}`, (err,hash) => {
-	let hash_data = JSON.stringify({name:VIDEO, hash:hash});
-	fs.writeFile(HASH, hash_data);
+let connections_map = new Map();
+let nameSet = new Set();
+// const storage = multer.diskStorage({
+// 		destination: function (req, file, cb) {
+// 				cb(null, './logs');
+// 		},
+// 		filename: function (req, file, cb) {
+// 				cb(null, `${Date.now()}_log.txt`);
+// 	}
+// });
+// const upload = multer({storage:storage});
+//DB setup
+const db = mongoose.connection;
+const deviceSchema = mongoose.Schema({
+  name: String,
+  location: Array,
+  cache:[{name:String, size:Number}],
+  d2d:Number
+});
+deviceSchema.set('collection', 'devices')
+const Device = mongoose.model('Device', deviceSchema);
+//DB start
+mongoose.connect('mongodb://localhost:27017/virtualcache');
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  // we're connected!
+  console.log("MongoDB connected");
 });
 
-let connections_map = new Map();
+//ROUTES
 app.get('/download', (req, res) => {
 	let currentDevice = req.headers['user-agent'];
 	//calculate the ratio
@@ -43,27 +68,27 @@ app.get('/download', (req, res) => {
 	});
 });
 
-let nameSet = new Set();
 app.post('/names', (req, res) => {
 	nameSet.add(req.body.name);
 	res.end();
 });
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, './logs');
-    },
-    filename: function (req, file, cb) {
-        cb(null, `${Date.now()}_log.txt`);
-  }
-})
-
-const upload = multer({storage:storage});
-
-app.post('/logs', upload.single('log'),(req, res) => {
-  // console.log(req.body); // form fields
-  // console.log(req.file); // form files
-  res.status(204).end();
+// app.post('/logs', upload.single('log'),(req, res) => {
+// 	// console.log(req.body); // form fields
+// 	// console.log(req.file); // form files
+// 	res.status(204).end();
+// });
+//Json version of logs
+app.post('/logs', (req, res) => {
+  res.end();
+  const device = new Device({
+    name: req.body.name,
+    location: req.body.location,
+    cache: req.body.cache,
+    d2d: req.body.d2d
+  });
+  console.log(req.body);
+  device.save().catch(err => console.error(err));
 });
 
 app.get('/logs', (req, res) => {
@@ -75,13 +100,12 @@ app.get('/logs', (req, res) => {
 	res.download(`./${DATA}`, `${Date.now()}_log.txt`, err => res.end());
 });
 
-app.get('/hash', (req, res) => res.download(`./${HASH}`, 'hash.txt', err => res.end()));
-
 app.get('/erase', (req, res) => {
 	connections_map.clear();
 	fs.writeFile(DATA,'', err => res.end());
 });
 
+//Server Startup
 app.listen(process.env.PORT || 3000,'0.0.0.0',() => {
 	console.log(`Example app listening on port ${process.env.PORT || 3000}!`);
 });
