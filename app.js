3589bd332fbe3828359770dac01783a9cda201b5
Edward Hu <hues@usc.edu>
2016-06-20 12:24:55 -0700

    geoSpatial Schema

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 #!/usr/bin/env nodejs
-'use strict'
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const app = express();
-const fs = require('fs');
-const MongoClient = require('mongodb').MongoClient
+const MongoClient = require('mongodb').MongoClient;
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');// Use bluebird
 const version = require('mongoose-version');
-
+const MONGO_URL = 'mongodb://localhost:27017/virtualcache';
 //SETUP CODE
 app.use('/', express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'pug');
@@ -17,39 +15,46 @@ app.use(bodyParser.urlencoded({
 	 extended: true
 }));
 app.use(bodyParser.json());
-let connections_map = new Map();
 
 //DB setup
 const db = mongoose.connection;
 const deviceSchema = mongoose.Schema({
 	name: String,
-	location: Array,
 	cache:[{name:String, size:Number}],
-	d2d:Number,
-	time:Number
+	d2d:0,
+	time:Number,
+	loc:Array
 });
 deviceSchema.plugin(version);
 deviceSchema.set('collection', 'devices')
 const Device = mongoose.model('Device', deviceSchema);
 //DB start
-mongoose.connect('mongodb://localhost:27017/virtualcache');
+mongoose.connect(MONGO_URL);
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
 	// we're connected!
-	console.log("MongoDB connected");
+	console.log('MongoDB connected');
 });
 
+var virtual_cache;
+MongoClient.connect(MONGO_URL, (err, database) => {
+  if(err) throw err;
+  virtual_cache = database;
+	//Server Startup
+	app.listen(process.env.PORT || 3000,'0.0.0.0',() => {
+		console.log(`Example app listening on port ${process.env.PORT || 3000}!`);
+	});
+});
 //ROUTES
+
 app.get('/', function (req, res) {
-	const currentDB_promise = Device.find().exec();
-	const historicalDB_promise =
-		MongoClient
-		.connect('mongodb://localhost:27017/virtualcache')
-		.then(db=>db.collection('versions'))
-		.then(collection=>collection.find())
-		.then(cursor=>cursor.toArray())
+	const currentDB = Device.find().exec();
+	const history = virtual_cache
+		.collection('versions')
+		.find()
+		.toArray()
 		.then(data=>JSON.stringify(data,null,4));
-	Promise.all([currentDB_promise,historicalDB_promise])
+	Promise.all([currentDB, history])
 		.then(arr=>res.render('index',{dbCurrent:arr[0], dbHistory:arr[1]}));
 });
 
@@ -62,22 +67,14 @@ app.post('/logs', (req, res) => {
 			const device = new Device(req.body);
 			device.save().catch(err=>console.error(err));
 		} else {
-			result.location = req.body.location;
-			result.cache = req.body.cache;
-			result.d2d = req.body.d2d;
-			result.time = req.body.time;
+			Object.assign(result,req.body);
 			result.save().catch(err=>console.error(err));
 		}
 	});
 });
 
-app.get('/db', (req, res) => {
-	Device.find().then( docs => {
-		res.send(docs);
-	});
-});
-
-//Server Startup
-app.listen(process.env.PORT || 3000,'0.0.0.0',() => {
-	console.log(`Example app listening on port ${process.env.PORT || 3000}!`);
+app.get('/erase', (req,res) => {
+	db.collection('versions').drop();
+	db.collection('devices').drop();
+	res.end();
 });
